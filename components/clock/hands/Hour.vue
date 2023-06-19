266381d303be4a1eb9ecff34be973ba08745152e
이준용 <iiiuooyoo@gmail.com>
2023-06-19 17:06:35 +0900

    feat : 시침 스타일 완료

--- components/clock/hands/Hour.vue
+++ components/clock/hands/Hour.vue
@@ -1,7 +1,13 @@
 <template>
-    <div class="clock-hands-hour"></div>
+    <div class="clock-hands-hour z-[7] w-[6px] h-[70px]"></div>
 </template>
 
 <script setup></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.clock-hands-hour {
+    background-color: #ff7f26;
+    transform-origin: bottom center;
+    transform: translate(-50%, -100%);
+}
+</style>
