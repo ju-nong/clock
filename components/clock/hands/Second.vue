a84456a4b23eea2f40c0cc11edbc4477e45d5194
이준용 <iiiuooyoo@gmail.com>
2023-06-19 17:03:59 +0900

    feat : 초침 스타일 완료

--- components/clock/hands/Second.vue
+++ components/clock/hands/Second.vue
@@ -1,7 +1,12 @@
 <template>
-    <div class="clock-hands-second"></div>
+    <div class="clock-hands-second z-[9] bg-red-700 w-[2px] h-[110px]"></div>
 </template>
 
 <script setup></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.clock-hands-second {
+    transform-origin: 50% 80%;
+    transform: translate(-50%, -80%) rotate(30deg);
+}
+</style>
