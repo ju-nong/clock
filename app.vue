dfc8e5f6b3c1db69c0b4b2849243d3d5e4ab8575
이준용 <iiiuooyoo@gmail.com>
2023-06-20 16:50:19 +0900

    feat : meta 태그 추가

--- app.vue
+++ app.vue
@@ -1,4 +1,30 @@
 <template>
+    <Head>
+        <Title>아날로그 시계</Title>
+        <Link rel="icon" type="image/png" href="/favicon.png" />
+        <Meta name="theme-color" content="#ff7f26" />
+        <Meta property="og:type" content="website" />
+        <Meta property="og:title" content="아날로그 시계" />
+        <Meta property="og:description" content="아날로그 시계를 만나보세요." />
+        <Meta property="og:locale" content="ko_KR" />
+        <Meta property="og:url" content="https://clock-analog.vercel.app/" />
+        <Meta property="og:image" content="/thumbnail.png" />
+        <Meta property="og:site_name" content="아날로그 시계" />
+
+        <Meta name="twitter:title" content="아날로그 시계" />
+        <Meta
+            name="twitter:description"
+            content="아날로그 시계를 만나보세요."
+        />
+        <Meta name="twitter:image" content="/thumbnail.png" />
+        <Meta name="twitter:card" content="summary_large_image" />
+        <Meta name="subject" content="아날로그 시계를 만나보세요." />
+        <Meta
+            name="keywords"
+            content="HTML, CSS, Javascript, Vue, Nuxt, TailwindCSS, Clock"
+        />
+        <Meta name="description" content="아날로그 시계를 만나보세요." />
+    </Head>
     <Clock />
 </template>
 
