8829536900a96618ae230203f019034eb7b95cce
이준용 <iiiuooyoo@gmail.com>
2023-06-20 18:53:14 +0900

    feat : Digital 시계 윤곽

--- components/clock/analog/Index.vue
+++ components/clock/analog/Index.vue
@@ -12,7 +12,7 @@
 <style lang="scss">
 .clock-analog {
     box-shadow: inset 0px 10px 10px rgba(0, 0, 0, 0.2),
-        0px 5px 10px rgba(0, 0, 0, 0.5); /* 안쪽 그림자 */
+        0px 5px 10px rgba(0, 0, 0, 0.5);
     border: 3px solid #fff;
 }
 </style>
--- components/clock/digital/Index.vue
+++ components/clock/digital/Index.vue
@@ -1,7 +1,19 @@
 <template>
-    <div>안녕</div>
+    <div class="clock-digital w-[250px] h-[250px] flex flex-col justify-center">
+        <div
+            class="clock-digital-container w-full h-[150px] rounded-3xl bg-white"
+        ></div>
+    </div>
 </template>
 
 <script setup></script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.clock-digital {
+    &-container {
+        box-shadow: inset 0px 10px 10px rgba(0, 0, 0, 0.2),
+            0px 5px 10px rgba(0, 0, 0, 0.5);
+        border: 3px solid #fff;
+    }
+}
+</style>
