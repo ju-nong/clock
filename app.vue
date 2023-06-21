05eaa189c4cfe24760b3ba656a0c1fbdb14562a3
이준용 <iiiuooyoo@gmail.com>
2023-06-21 09:41:24 +0900

    feat : Meta 태그 수정

--- app.vue
+++ app.vue
@@ -1,29 +1,26 @@
 <template>
     <Head>
-        <Title>아날로그 시계</Title>
+        <Title>Clock</Title>
         <Link rel="icon" type="image/png" href="/favicon.png" />
         <Meta name="theme-color" content="#ff7f26" />
         <Meta property="og:type" content="website" />
-        <Meta property="og:title" content="아날로그 시계" />
-        <Meta property="og:description" content="아날로그 시계를 만나보세요." />
+        <Meta property="og:title" content="Clock" />
+        <Meta property="og:description" content="시계를 만나보세요." />
         <Meta property="og:locale" content="ko_KR" />
         <Meta property="og:url" content="https://clock-analog.vercel.app/" />
         <Meta property="og:image" content="/thumbnail.png" />
-        <Meta property="og:site_name" content="아날로그 시계" />
+        <Meta property="og:site_name" content="Clock" />
 
-        <Meta name="twitter:title" content="아날로그 시계" />
-        <Meta
-            name="twitter:description"
-            content="아날로그 시계를 만나보세요."
-        />
+        <Meta name="twitter:title" content="Clock" />
+        <Meta name="twitter:description" content="시계를 만나보세요." />
         <Meta name="twitter:image" content="/thumbnail.png" />
         <Meta name="twitter:card" content="summary_large_image" />
-        <Meta name="subject" content="아날로그 시계를 만나보세요." />
+        <Meta name="subject" content="시계를 만나보세요." />
         <Meta
             name="keywords"
-            content="HTML, CSS, Javascript, Vue, Nuxt, TailwindCSS, Clock"
+            content="HTML, CSS, Javascript, Vue, Nuxt, TailwindCSS, Clock, Analog, Digital"
         />
-        <Meta name="description" content="아날로그 시계를 만나보세요." />
+        <Meta name="description" content="시계를 만나보세요." />
     </Head>
     <Clock />
 </template>
