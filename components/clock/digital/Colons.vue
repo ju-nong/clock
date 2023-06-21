e94dde0a4c3eb606de1f48711a98b11edebce43e
이준용 <iiiuooyoo@gmail.com>
2023-06-21 10:51:10 +0900

    feat : Digital / 콜론 추가

---
+++ components/clock/digital/Colons.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="clock-digital-colons flex flex-col gap-y-[30px]">
+        <div></div>
+        <div></div>
+    </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss">
+.clock-digital-colons {
+    > div {
+        width: 10px;
+        height: 10px;
+        background-color: #ff7f26;
+        border-radius: 100%;
+    }
+}
+</style>
